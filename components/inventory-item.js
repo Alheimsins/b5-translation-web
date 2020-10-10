470475ec797a36f51169e9f5b80dfc8b209ed086
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-10-10 12:51:35 +0200

    for => htmlFor (patch)

--- components/inventory-item.js
+++ components/inventory-item.js
@@ -1,12 +1,9 @@
-import { nanoid } from 'nanoid'
-
 const Item = props => {
-  const id = nanoid()
-  const { text, dataType } = props
+  const { id, text, dataType } = props
   const placeholder = `Your translation of: ${text}`
   return (
     <div className='mt-2'>
-      <label for={id} className='mb-1 font-semibold'>{text}</label>
+      <label htmlFor={id} className='mb-1 font-semibold'>{text}</label>
       <input id={id} type='text' data-type={dataType} placeholder={placeholder} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' />
     </div>
   )
